be07e3064b69292bbc0825f2ac433fc21505e583
HolyMC2 <marcoapv@hotmail.com>
2022-09-26 10:22:01 -0600

    change bar tags

--- rc.lua
+++ rc.lua
@@ -200,7 +200,7 @@ end
      -- set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "control", "2", "3", "4", "5", "6", "p-chat", "personal", "mail" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
