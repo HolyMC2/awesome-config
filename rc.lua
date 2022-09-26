f93c2a9337127abde8daa9064cd14f378bc6f9b7
HolyMC2 <marcoapv@hotmail.com>
2022-09-26 14:19:32 -0600

    add awesomewm bar tags

--- rc.lua
+++ rc.lua
@@ -200,7 +200,7 @@ end
      -- set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "control", "2", "3", "4", "5", "6", "p-chat", "personal", "mail" }, s, awful.layout.layouts[1])
+    awful.tag({ "control", "2", "3", "4", "5", "ventas", "p-chat", "personal", "mail" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
